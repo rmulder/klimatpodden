3df048151709db4f7ca8b3d496d3de019531966c
Robert Matthew Mulder <rmmulder@gmail.com>
2020-12-13 20:49:01 -0500

    changes

--- post-export.js
+++ post-export.js
@@ -1,7 +1,8 @@
 // copy _redirects file to ./out folder
 const fs = require("fs");
 // destination.txt will be created or overwritten by default.
-fs.copyFile("_redirects", "./out/_redirects", err => {
+/*fs.copyFile("_redirects", "./out/_redirects", err => {
   if (err) throw err;
   console.log("_redirects was copied to ./out/_redirects");
 });
+*/
